00fe16843a773f2418eb91eb3b7350227866f24b
ridhopratama17 <ridhopratamaramdany@gmail.com>
2023-10-20 07:49:25 +0700

    Tugas 1-8

--- index.js
+++ index.js
@@ -22,3 +22,57 @@ app.put('/user', (req, res) => {
 app.delete('/user', (req, res) => {
     res.send('  DELETE request at /user')
 })
+//  Bagian nomer 5
+const data_users = [
+    // = port/1 etc
+    { id: 1, name: "Ilham Anugrah", alamat: "Sukabumi" },
+    { id: 2, name: "Ghina Khairunnisa", alamat: "Bandung" },
+    { id: 3, name: "Hana Syifa", alamat: "Jakarta" },
+    { id: 4, name: "Daehan Ibrahim", alamat: "Bandung" },
+];
+
+app.get("/users", (req, res) => {
+    //mendapatkan data dari database
+    const data = data_users; // understablee
+    let result = {
+        status: 200,
+        data: data,
+    };
+
+    res.json(result);
+});
+
+app.get("/users/:id", (req, res) => {
+    // get data dari parameter
+    let id = parseInt(req.params.id);
+    // DARI DATABASE    
+    let result;
+    const user = data_users.find((user) => user.id === id);
+    if (user) {
+        result = {
+            status: 200,
+            data: user,
+        };
+    }
+    res.json(result);
+});
+
+app.get("/users/:id", (req, res) => {
+    // get data dari parameter
+    let id = parseInt(req.params.id);
+
+    // get data dari database
+    let result;
+    const user = data_users.find((user) => user.id === id);
+    if (user) {
+        result = {
+            status: 200,
+            data: user,
+        };
+    } else {
+        res.status(404).json({ error: "User not found" });
+    }
+    res.json(result);
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
